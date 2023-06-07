1db6f8329b85180307c489b624c84f1eb3dd945b
Irontriston <66928086+Irontriston@users.noreply.github.com>
2023-06-07 19:16:32 -0400

    Add files via upload

---
+++ mod_icons.lua
@@ -0,0 +1,7 @@
+-- Manually integrates custom strategic icons.
+UnitIconAssignments = {
+	{BlueprintId = 'UEL0001', IconSet = 'icon_commander_uef'},
+	{BlueprintId = 'UAL0001', IconSet = 'icon_commander_aeon'},
+	{BlueprintId = 'URL0001', IconSet = 'icon_commander_cybran'},
+	{BlueprintId = 'XSL0001', IconSet = 'icon_commander_seraphim'},
+}
---
+++ mod_info.lua
@@ -0,0 +1,8 @@
+name = "General Reworks"
+uid = "4bebc675-89a6-4263-b26e-8aa41ec27863"
+version = 1.0
+description = "A large mod that makes wide sweeping changes across the game, from generally shrinking skirts across all buildings to making the seraphim canonically overpowered."
+author = "IronTristonia"
+exclusive = false
+ui_only = false
+icon = ""
