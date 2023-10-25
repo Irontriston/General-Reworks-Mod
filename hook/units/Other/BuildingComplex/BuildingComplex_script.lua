625d4743463d54309a4c14c96024e972d3dce5a7
Irontriston <66928086+Irontriston@users.noreply.github.com>
2023-10-25 19:17:34 -0400

    Delete hook/units directory
    Wrong place...

--- hook/units/Other/BuildingComplex/BuildingComplex_script.lua
+++
@@ -1,5 +0,0 @@
---Building Complex
-local Struct = import('/lua/DefaultUnits.lua').StructureUnit
-BuildingComplex = Class(Struct) {
-}
-TypeClass = BuildingComplex
--- hook/units/Other/Hooman/Human_script.lua
+++
@@ -1,7 +0,0 @@
---Normal hooman by IronTriston/JiouMu
-local Walky = import('/lua/defaultunits.lua').WalkingLandUnit
-local Buildy = import('/lua/defaultunits.lua').ConstructionUnit
-
-Human = Class(Buildy) {
-}
-TypeClass = Human
