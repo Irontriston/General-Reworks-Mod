0c3f7d8ccb471aab2cb6039e7254ce0f8dad5c9f
Irontriston <66928086+Irontriston@users.noreply.github.com>
2023-10-25 20:24:20 -0400

    Cybran Construction Add-ins Finale

---
+++ units/Cybran/Const/XRB0104/XRB0104_script.lua
@@ -0,0 +1,15 @@
+-- ****************************************************************************
+-- **
+-- **  File     :  /cdimage/units/XRB0204/XRB0204_script.lua
+-- **  Author(s):  Dru Staltman
+-- **
+-- **  Summary  :  Cybran Engineering tower
+-- **
+-- **  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
+-- ****************************************************************************
+local CConstructionStructureUnit = import('/lua/cybranunits.lua').CConstructionStructureUnit
+
+XRB0104 = Class(CConstructionStructureUnit) {
+}
+
+TypeClass = XRB0104
